32f1b86ae583ddf90ccd9c58c0d929e59e665731
SungBin <40740128+sungbin5304@users.noreply.github.com>
2019-09-15 22:08:53 +0900

    Add files via upload

---
+++ ReadMoreUtils.kt
@@ -0,0 +1,85 @@
+package com.sungbin.reply.bot.utils
+
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.sungbin.reply.bot.R
+
+object ReadMoreUtils {
+    fun setReadMoreLine(view: TextView, text: String, maxLine: Int,
+                        expanedText: String = "...더보기",
+                        expanedTextColor: Int = ContextCompat.getColor(view.context, R.color.colorPrimary)) {
+        view.text = text
+        view.post {
+            if (view.lineCount >= maxLine) {
+                val lineEndIndex = view.layout.getLineVisibleEnd(maxLine - 1)
+
+                val split = text.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                var splitLength = 0
+
+                var lessText = ""
+                for (item in split) {
+                    splitLength += item.length + 1
+                    if (splitLength >= lineEndIndex) {
+                        lessText += if (item.length >= expanedText.length) {
+                            item.substring(0, item.length - expanedText.length) + expanedText
+                        } else {
+                            item + expanedText
+                        }
+                        break
+                    }
+                    lessText += item + "\n"
+                }
+                val spannableString = SpannableString(lessText)
+                spannableString.setSpan(
+                        object : ClickableSpan() {
+                            override fun onClick(vew: View) {
+                                view.text = text
+                            }
+
+                            override fun updateDrawState(ds: TextPaint) {
+                                ds.color = expanedTextColor
+                            }
+                        },
+                        spannableString.length - expanedText.length,
+                        spannableString.length,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                view.text = spannableString
+                view.movementMethod = LinkMovementMethod.getInstance()
+            } else view.text = text
+        }
+    }
+
+    fun setReadMoreLength(view: TextView, text: String, maxLength: Int,
+                          expanedText: String = "...더보기",
+                          expanedTextColor: Int = ContextCompat.getColor(view.context, R.color.colorPrimary)) {
+        view.post {
+            if (view.length() > maxLength) {
+                val lestText = text.substring(0, maxLength) + expanedText
+                val spannableString = SpannableString(lestText)
+                spannableString.setSpan(
+                        object : ClickableSpan() {
+                            override fun onClick(vew: View) {
+                                view.text = text
+                            }
+
+                            override fun updateDrawState(ds: TextPaint) {
+                                ds.color = expanedTextColor
+                            }
+                        },
+                        spannableString.length - expanedText.length,
+                        spannableString.length,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                view.text = spannableString
+                view.movementMethod = LinkMovementMethod.getInstance()
+            } else view.text = text
+        }
+    }
+}
